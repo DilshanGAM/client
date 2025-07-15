379505315cd0c1db5b91cb4b8aac49fcc72a5b0c
DilshanGAM <it21180934@my.sliit.lk>
2025-07-15 13:43:43 +0530

    feedback update ✏️

--- src/app/assets/data/reviews.ts
+++ src/app/assets/data/reviews.ts
@@ -1,80 +1,121 @@
+// const reviews = [
+//   {
+//     name: "Pasan Pramuditha",
+//     username: "undergraduate",
+//     body: "වැඩ එක්ක මුල ඉදලම recording  බැලුවේ  live එන්න බැරිවුනා 🥲. මේ වගේ පොඩි ගානකට ඇත්තටම මුදලට වඩා ලොකු දෙයක් කියල දුන්නා ගොඩක් පැහැදිලිව .හොද support එකකුත් දුන්න.ගොඩක් අලුත් දේවල් ඉගෙනගත්තා.Thankyou sir.❤🎊",
+//     img: "/23.png", // Local image path directly in public folder
+//   },
+//   {
+//     name: "Ramya Withana",
+//     username: "entrepreneur",
+//     body: "Highly recommended course and highly recommended teacher. Godak watinwa me course eka. Sir godak pahadiliwa hama deykma explain karnawa. 👌👌👌",
+//     img: "/2.jpg", // Local image path directly in public folder
+//   },
+//   {
+//     name: "Dilan Shanuka",
+//     username: "undergraduate",
+//     body: "සිංහලෙන් පැහැදිලිව ඉගෙන ගන්න තියෙන එක පට්ට වටිනව ඇත්තටම. 💯 Guide කරන කෙනත් Teaching Expert කෙනෙක් තමයි ඉතින් 😊👌",
+//     img: "/3.jpg", // Local image path directly in public folder
+//   },
+//   // {
+//   //   name: "Sumudu Nisansala",
+//   //   username: "Analyst",
+//   //   body: "Hi Malith. Ada session eka aththatama niyamai. Hondata therenawa kiyala dena dewal. Mama IT degree eka kare. Horizon eke. Analyst kenek widihata den work karanawa Ascent eke. Kisima deyak danne nethi kenekta unath hondatama clear oya kiyala dena widiha. Mama godak project karala thiyenawa ath kisima kammelikamak nethuwa ahan hitiya. Oyagen ganna godak loku knowledge ekak thiyanawa. Thank you very much hemadema kiyala denawata. Mage track eka change karanna oyage course eka loku support ekak. ❤",
+//   //   img: "/9.jpg", // Local image path directly in public folder
+//   // },
+//   {
+//     name: "Shanuka Imantha",
+//     username: "undergraduate",
+//     body: "thank you very much sir💗 Course එක ගැන කියන්න කිසිම අඩුපාඩුවක් නෑ. ඕනම වෙලාවක error එකක් ආපු වෙලවකට anydesk එකෙන් මාරම Support එකක් දුන්නා. ඇත්තම කිව්වොත් පොඩි මුදලකට මෙච්චර වැඩක් කරපු මන් දැක්ක එකම තැන. Content එකත් හොඳටම කියලා දෙනවා. ආයෙත් කියන්න ඔනේ Thank you very much sir💐💗🌸",
+//     img: "/22.png", // Local image path directly in public folder
+//   },
+//   {
+//     name: "Aseka Dahanayake",
+//     username: "undergraduate",
+//     body: "හැමදේම සරල විදියටම පැහැදිලි කරල ඇඟට දැනෙන විදියට කියල දෙන්න පුදුම හැකියාවක් තියන ගුරුවරයෙක්. ❤ Highly recommended ! 🔥💯",
+//     img: "/5.jpg", // Local image path directly in public folder
+//   },
+//   {
+//     name: "Sachintha Bimsara",
+//     username: "software engineer",
+//     body: "Highly recommended 🔥💯",
+//     img: "/6.jpg", // Local image path directly in public folder
+//   },
+//   {
+//     name: "Shasthri Kawya",
+//     username: "undergraduate",
+//     body: "Hey, check out this Full-Stack Web Development Certificate Course! It covers everything from frontend to backend, and even AI-driven coding techniques. Perfect for beginners or anyone looking to start freelancing! 💻..highly recommend...",
+//     img: "/7.jpg", // Local image path directly in public folder
+//   },
+//   {
+//     name: "Oshan Amarathunga",
+//     username: "undergraduate",
+//     body: "I highly recommend this, course . You can get more knowledge on the latest stacks, with hands-on real world project . Valuable for this price ....",
+//     img: "/8.jpg", // Local image path directly in public folder
+//   },
+//   {
+//     name: "Vindya Sathsarani",
+//     username: "undergraduate",
+//     body: "Thank you so much sir ❤ course එක ගැන කියන්න වචන නෑ..නියමයි ❤ ගොඩක් දේවල් ඉගෙනගත්තා සර්ගෙන්..සර්ගේ ඉදිරි වැඩ වලට සුභ පතනවා 🥰",
+//     img: "/24.png", // Local image path directly in public folder
+//   },
+//   {
+//     name: "Dilshan Warnasuriya",
+//     username: "undergraduate",
+//     body: `කෙටියෙන්ම කිව්වොත් course එක supiri.. ♥
+
+// කර කර හිටිය සමහර දෙවල් ඇයි එහෙම කරන්නෙ කියල හරියටම දැනගත්තෙ මෙතනින්. එ වගෙම නොදන්න දෙවල් ගොඩක් ඉගෙනගත්ත. සර් කියල දුන්න විදිහටත් හොඳට තෙරුනා.
+
+// හැමදේටම ගොඩක් ස්තුති සර් ..`,
+//     img: "/25.png", // Local image path directly in public folder
+//   },
+//   {
+//     name: "Oshitha Dewruwan",
+//     username: "undergraduate",
+//     img: "/26.png", // Local image path directly in public folder
+//     body: `අවුරුදු ගානක් තිස්සෙ ඉංග්‍රීසි වීඩියෝ බල බල තේරුම් ගන්න ට්‍රයි කරා ඒත් හරියට අල්ලගන්න බැරිවුනා. සර් නියමෙට ඒ ටික කියල දුන්න සරලවම. දැන් හරි ආසාවෙන් කෝඩ් කරන්නෙ. මුදලට එහා ගිය ලොකු උදව්වක් 🫡`
+//   }
+// ];
 
 const reviews = [
+	{
+		name: "Nandun Perera",
+		username: "undergraduate",
+		img: "/2.jpg",
+		body: "The teaching approach of instructor is excellent — simplifies all the concepts with real-world examples. As a past student I can say This is a 'Worth recommending!' ❤️",
+	},
+	{
+		name: "Abhishek Peiris",
+		username: "undergraduate",
+		img: "/3.jpg",
+		body: "Highly recommended! I have attended several courses from him — he explains everything in a short, clear, and simple way 🔥❤️",
+	},
+	{
+		name: "Chirath Themiya",
+		username: "undergraduate",
+		img: "/4.jpg",
+		body: `හැමදේම පැහැදිලිව කියලා දෙනවා .. ඒ විතරක් නෙමේ අමාරු පාඩම් ඉතා simple විදියට කරනවා
+Highly recommended 💫`,
+	},
+	{
+		name: "Karindra Gimhan",
+		username: "undergraduate",
+		img: "/5.jpg",
+		body: `Course එක guide කරන කෙනා teaching expert කෙනෙක් කලින් courses වලට join වෙලා තියෙනවා වැඩේ හොදට වෙනවා ...
+Highly recommended 🫡❤️`,
+	},
+	{
+		name: "Wimukthi Gunarathna",
+		username: "undergraduate",
+		img: "/8.jpg",
+		body: `මුල ඉදන් අගට complex concepts simple විදියට explain කරනවා .. Real world problem අර ගෙන හොදටම explain කරනවා...
+Recommended ❤️`,
+	},
   {
-    name: "Pasan Pramuditha",
-    username: "undergraduate",
-    body: "වැඩ එක්ක මුල ඉදලම recording  බැලුවේ  live එන්න බැරිවුනා 🥲. මේ වගේ පොඩි ගානකට ඇත්තටම මුදලට වඩා ලොකු දෙයක් කියල දුන්නා ගොඩක් පැහැදිලිව .හොද support එකකුත් දුන්න.ගොඩක් අලුත් දේවල් ඉගෙනගත්තා.Thankyou sir.❤🎊",
-    img: "/23.png", // Local image path directly in public folder
-  },
-  {
-    name: "Ramya Withana",
-    username: "entrepreneur",
-    body: "Highly recommended course and highly recommended teacher. Godak watinwa me course eka. Sir godak pahadiliwa hama deykma explain karnawa. 👌👌👌",
-    img: "/2.jpg", // Local image path directly in public folder
-  },
-  {
-    name: "Dilan Shanuka",
-    username: "undergraduate",
-    body: "සිංහලෙන් පැහැදිලිව ඉගෙන ගන්න තියෙන එක පට්ට වටිනව ඇත්තටම. 💯 Guide කරන කෙනත් Teaching Expert කෙනෙක් තමයි ඉතින් 😊👌",
-    img: "/3.jpg", // Local image path directly in public folder
-  },
-  // {
-  //   name: "Sumudu Nisansala",
-  //   username: "Analyst",
-  //   body: "Hi Malith. Ada session eka aththatama niyamai. Hondata therenawa kiyala dena dewal. Mama IT degree eka kare. Horizon eke. Analyst kenek widihata den work karanawa Ascent eke. Kisima deyak danne nethi kenekta unath hondatama clear oya kiyala dena widiha. Mama godak project karala thiyenawa ath kisima kammelikamak nethuwa ahan hitiya. Oyagen ganna godak loku knowledge ekak thiyanawa. Thank you very much hemadema kiyala denawata. Mage track eka change karanna oyage course eka loku support ekak. ❤",
-  //   img: "/9.jpg", // Local image path directly in public folder
-  // },
-  {
-    name: "Shanuka Imantha",
-    username: "undergraduate",
-    body: "thank you very much sir💗 Course එක ගැන කියන්න කිසිම අඩුපාඩුවක් නෑ. ඕනම වෙලාවක error එකක් ආපු වෙලවකට anydesk එකෙන් මාරම Support එකක් දුන්නා. ඇත්තම කිව්වොත් පොඩි මුදලකට මෙච්චර වැඩක් කරපු මන් දැක්ක එකම තැන. Content එකත් හොඳටම කියලා දෙනවා. ආයෙත් කියන්න ඔනේ Thank you very much sir💐💗🌸",
-    img: "/22.png", // Local image path directly in public folder
-  },
-  {
-    name: "Aseka Dahanayake",
-    username: "undergraduate",
-    body: "හැමදේම සරල විදියටම පැහැදිලි කරල ඇඟට දැනෙන විදියට කියල දෙන්න පුදුම හැකියාවක් තියන ගුරුවරයෙක්. ❤ Highly recommended ! 🔥💯",
-    img: "/5.jpg", // Local image path directly in public folder
-  },
-  {
-    name: "Sachintha Bimsara",
-    username: "software engineer",
-    body: "Highly recommended 🔥💯",
-    img: "/6.jpg", // Local image path directly in public folder
-  },
-  {
-    name: "Shasthri Kawya",
-    username: "undergraduate",
-    body: "Hey, check out this Full-Stack Web Development Certificate Course! It covers everything from frontend to backend, and even AI-driven coding techniques. Perfect for beginners or anyone looking to start freelancing! 💻..highly recommend...",
-    img: "/7.jpg", // Local image path directly in public folder
-  },
-  {
-    name: "Oshan Amarathunga",
-    username: "undergraduate",
-    body: "I highly recommend this, course . You can get more knowledge on the latest stacks, with hands-on real world project . Valuable for this price ....",
-    img: "/8.jpg", // Local image path directly in public folder
-  },
-  {
-    name: "Vindya Sathsarani",
-    username: "undergraduate",
-    body: "Thank you so much sir ❤ course එක ගැන කියන්න වචන නෑ..නියමයි ❤ ගොඩක් දේවල් ඉගෙනගත්තා සර්ගෙන්..සර්ගේ ඉදිරි වැඩ වලට සුභ පතනවා 🥰",
-    img: "/24.png", // Local image path directly in public folder
-  },
-  {
-    name: "Dilshan Warnasuriya",
-    username: "undergraduate",
-    body: `කෙටියෙන්ම කිව්වොත් course එක supiri.. ♥
-
-කර කර හිටිය සමහර දෙවල් ඇයි එහෙම කරන්නෙ කියල හරියටම දැනගත්තෙ මෙතනින්. එ වගෙම නොදන්න දෙවල් ගොඩක් ඉගෙනගත්ත. සර් කියල දුන්න විදිහටත් හොඳට තෙරුනා. 
-
-හැමදේටම ගොඩක් ස්තුති සර් ..`,
-    img: "/25.png", // Local image path directly in public folder
-  },
-  {
-    name: "Oshitha Dewruwan",
-    username: "undergraduate",
-    img: "/26.png", // Local image path directly in public folder
-    body: `අවුරුදු ගානක් තිස්සෙ ඉංග්‍රීසි වීඩියෝ බල බල තේරුම් ගන්න ට්‍රයි කරා ඒත් හරියට අල්ලගන්න බැරිවුනා. සර් නියමෙට ඒ ටික කියල දුන්න සරලවම. දැන් හරි ආසාවෙන් කෝඩ් කරන්නෙ. මුදලට එහා ගිය ලොකු උදව්වක් 🫡`
+    name: "Malith Dilshan",
+    username: "Software Engineer and Teacher",
+    img: "/9.jpg",
+    body: `Highly recommend this course! The instructor explains complex topics in a simple way, making it easy to understand.`
   }
 ];
 
